f1bb71bc16f73709f68751cd07dfe494a9818971
Petr Weissar <weissar.petr@gmail.com>
2018-07-08 13:48:23 +0200

    Drawer

--- app/src/main/java/cz/uhk/teeter/CanvasActivity.java
+++ app/src/main/java/cz/uhk/teeter/CanvasActivity.java
@@ -6,7 +6,10 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.annotation.Nullable;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Gravity;
 import android.view.MotionEvent;
 import android.view.SurfaceView;
 import android.view.View;
@@ -37,6 +40,9 @@ public class CanvasActivity extends AppCompatActivity {
 
     private Level level;
 
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -53,12 +59,16 @@ public class CanvasActivity extends AppCompatActivity {
         surfaceView = findViewById(R.id.surfaceView);
         Toast.makeText(this, getResources().getString(R.string.tap_to_play), Toast.LENGTH_SHORT).show();
 
-        findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
+        drawerLayout = findViewById(R.id.drawer_layout);
+        drawerLayout.setScrimColor(Color.TRANSPARENT);
+        navigationView = findViewById(R.id.nav_view);
+        navigationView.getHeaderView(0).findViewById(R.id.fab).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 sensorHandler.lockSphere();
             }
         });
+        drawerLayout.openDrawer(Gravity.LEFT);
 
         handler = new Handler();
         sensorHandler = new SensorHandler();
@@ -160,6 +170,15 @@ public class CanvasActivity extends AppCompatActivity {
         handler.removeCallbacks(runnable);
         init = false;
         super.onPause();
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (drawerLayout.isDrawerOpen(Gravity.LEFT)) {
+            super.onBackPressed();
+        } else {
+            drawerLayout.openDrawer(Gravity.LEFT);
+        }
     }
 
     private void draw() {
